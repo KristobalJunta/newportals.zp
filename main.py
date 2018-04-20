4099bbc04f1c329f74fa7e401f042e8b40f86c80
Kristobal <junta.kristobal@gmail.com>
2018-04-20 14:46:30 +0300

    check message duplication

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from config import config
 client = TelegramClient('telethon', config.get('api_id'), config.get('api_hash'))
 client.start()
 
-messages = client.get_messages(config.get('channel_from'))
+msg_from = client.get_messages(config.get('channel_from'))
+msg_to = client.get_messages(config.get('channel_from'))
 
-for message in messages:
+for message in msg_from:
     if 'Запорізька' in message.message:
-        client.forward_messages(config.get('channel_to'), message)
+        if message not in msg_to:
+            client.forward_messages(config.get('channel_to'), message)
