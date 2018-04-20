011f4d79870d6066f8663af8c2c0b0eabee3c633
Kristobal <junta.kristobal@gmail.com>
2018-04-20 15:44:23 +0300

    check message duplication #2

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 
 from telethon import TelegramClient
 from config import config
+import os
+import json
 
-client = TelegramClient('telethon', config.get('api_id'), config.get('api_hash'))
+client = TelegramClient(os.path.abspath('telethon.session'), config.get('api_id'), config.get('api_hash'))
 client.start()
+
+sent_ids = json.load(open('sent.json', 'r'))
 
 msg_from = client.get_messages(config.get('channel_from'))
 msg_to = client.get_messages(config.get('channel_from'))
 
 for message in msg_from:
     if 'Запорізька' in message.message:
-        if message not in msg_to:
+        if message.id not in sent_ids:
+            sent_ids.append(message.id)
             client.forward_messages(config.get('channel_to'), message)
+
+json.dump(sent_ids, open('sent.json', 'w'))
